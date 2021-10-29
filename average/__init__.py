a01e72a9d7222f6eb7ac58a349f1be8e406481e9
Avery Khoo <averykhoo@gmail.com>
2021-10-29 19:20:13 +0800

    add some stuff to init so it's not empty

---
+++ average/__init__.py
@@ -0,0 +1,9 @@
+from average.generalized_mean import generalized_mean
+from average.lehmer_mean import lehmer_mean
+from average.log_mean import log_mean
+
+__all__ = (
+    generalized_mean,
+    lehmer_mean,
+    log_mean,
+)
